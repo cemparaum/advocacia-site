bb951ae90273d79e1e72c2ec3dbe34eae208c584
Daniel Andrade <cemparaum@gmail.com>
2026-07-10 22:46:32 -0300

    feat: add About and Header components and update deployment site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rodriguesfontenelle.com.br',
+  site: 'https://cemparaum.github.io',
+  base: '/advocacia-site',
   
   vite: {
     plugins: [tailwindcss()]
